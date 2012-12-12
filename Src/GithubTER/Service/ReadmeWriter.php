72a071b7dea7b952729743a32cc85cec96a3a06c
Georg Ringer <georg.ringer@gmail.com>
2012-12-12 02:05:05 +0100

    Readme writer

---
+++ Src/GithubTER/Service/ReadmeWriter.php
@@ -0,0 +1,87 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *  (c) 2012 Philipp Bergsmann <p.bergsmann@opendo.at>
+ *  All rights reserved
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+
+/**
+ * Writes a basic Readme.md for the git project
+ *
+ * @author    Georg Ringer
+ */
+namespace GithubTER\Service;
+
+class ReadmeWriter {
+
+	const ENDLINE_IDENTIFIER = '-- never edit the line below (typo3-ter) --';
+
+	/**
+	 * @var \GithubTER\Domain\Model\Extension
+	 */
+	protected $extension = '';
+
+	public function __construct($extension) {
+		$this->extension = $extension;
+	}
+
+	public function write() {
+		if (!is_file($this->getFilePath())) {
+			$this->generateNewFile();
+		} else {
+			$this->updateFile();
+		}
+	}
+
+	protected function generateNewFile() {
+		$content = $this->getContent();
+		file_put_contents($this->getFilePath(), $content);
+	}
+
+	protected function updateFile() {
+		$content = '';
+		$previousContent = file_get_contents($this->getFilePath());
+		$endLineIdentiferPosition = strpos($previousContent, self::ENDLINE_IDENTIFIER);
+
+		if ($endLineIdentiferPosition === FALSE) {
+			$content = $this->getContent() . $previousContent;
+		} else {
+			$trimmedReadme = substr($previousContent, $endLineIdentiferPosition + strlen(self::ENDLINE_IDENTIFIER));
+			$content = $this->getContent() . $trimmedReadme;
+		}
+
+		file_put_contents($this->getFilePath(), $content);
+	}
+
+	protected function getContent() {
+		$content = '
+		## TYPO3 Extension \'' . $this->extension->getKey() . '\' ##
+
+
+		';
+		$content .= self::ENDLINE_IDENTIFIER;
+		return $content;
+	}
+
+	/**
+	 * Get the file path to the readme file
+	 *
+	 * @return string
+	 */
+	protected function getFilePath() {
+		return getcwd() . '/Temp/Extension/' . $this->extension->getKey() . '/README.md';
+	}
+}
