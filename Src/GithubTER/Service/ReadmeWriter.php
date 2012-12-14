7672f044e9b3f216741f9c475c9cd4e10bfa8eec
Georg Ringer <georg.ringer@gmail.com>
2012-12-14 07:44:31 +0100

    improve readme

--- Src/GithubTER/Service/ReadmeWriter.php
+++ Src/GithubTER/Service/ReadmeWriter.php
@@ -78,23 +78,39 @@ class ReadmeWriter {
 	 * @return string
 	 */
 	protected function getContent() {
-		$content = '## TYPO3 Extension \'' . $this->extension->getKey() . '\' ##' . LF;
-		$content .= $this->version->getUploadComment() . LF;
-
 		$dataContent = '';
 		$dataTable = array(
 			array('Version', $this->version->getNumber() . ' ' . $this->version->getState()),
 			array('Release date', date($this->version->getUploadDate()), 'd.M Y'),
-			array('Author', $this->version->getAuthor()),
+			array('Author', $this->version->getAuthor()->getName()),
+			array('Comment', $this->version->getUploadComment()),
 		);
 		foreach ($dataTable as $data) {
-			$dataContent .= '<tr><td>' . $data[0] . '</td><td>' . $data[1] . '</td></tr>';
+			if (!empty($data[1])) {
+				$dataContent .= TAB . '<tr><td>' . $data[0] . '</td><td>' . $data[1] . '</td></tr>' . LF;
+			}
 		}
 		if (!empty($dataContent)) {
-			$content .= '<table>' . $dataContent . '</table>';
+			$dataContent .= LF . '<table>' . LF . $dataContent . '</table>';
 		}
 
-		$content .= self::ENDLINE_IDENTIFIER;
+		$content = '# TYPO3 Extension "' . $this->extension->getKey() . '"
+' . $this->extension->getDescription() . '
+
+## Version ' . $this->version->getNumber() . '
+
+
+' . $dataContent . '
+
+## !! Attention !!
+This is an **automatically** generated git version, based on the release into the [TYPO3 Extension Repository](http://www.typo3.org/extensions/).
+You can find the original version at http://typo3.org/extensions/repository/view/' . $this->extension->getKey() . '/ .
+
+It does not make sense to make pull requests as this repository has been created automatically from 3rd party, not from the original author(s).
+
+Every version of the extension is tagged with the version number, therefore you can switch quite easily between different versions.';
+
+		$content .= LF . LF . LF . self::ENDLINE_IDENTIFIER;
 		return $content;
 	}
 
