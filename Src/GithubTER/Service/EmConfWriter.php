3d9845deb563894a5228c8d48282a8df46590947
pbergsmann <p.bergsmann@opendo.at>
2013-01-17 10:22:59 +0100

    [BUGFIX] Changes emConf-Writer
    * Returns an empty array instead of a string
    
    Related: #3

--- Src/GithubTER/Service/EmConfWriter.php
+++ Src/GithubTER/Service/EmConfWriter.php
@@ -77,7 +77,7 @@ $EM_CONF[$_EXTKEY] = ' . $emConf . ';
 				}
 			}
 			if (!isset($emConf['constraints']) || !isset($emConf['constraints']['conflicts'])) {
-				$emConf['constraints']['conflicts'] = $this->dependencyToString($emConf['conflicts']);
+				$emConf['constraints']['conflicts'] = array();
 			}
 			if (!isset($emConf['constraints']) || !isset($emConf['constraints']['suggests'])) {
 				$emConf['constraints']['suggests'] = array();
